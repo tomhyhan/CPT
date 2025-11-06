530fe9008721f60c52fcabd274936623b074cf6c
tomhyhan <tomhyhan@gmail.com>
2025-11-06 11:58:17 -0800

    added search filter on description

--- backend/api/views/products.py
+++ backend/api/views/products.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 from api.models.products import ProductModel
@@ -8,8 +9,10 @@ from api.filters.products import ProductFilter
 class ProductList(generics.ListAPIView):
   serializer_class = ProductSerializer
   # DjangoFilterBackend automatically takes care of filtering queries
-  filter_backends = [DjangoFilterBackend]
+  # SearchFilter supports searching on TextField
+  filter_backends = [DjangoFilterBackend, SearchFilter]
   filterset_class = ProductFilter
+  search_fields = ["description"]
 
   def get_queryset(self):
     # This avoids extra database hits by pre-joining category and caching the result to make a query from tag model
